cb6b297bfc2b96c01ce6d608b5acfa4a32c5f7a0
Taber McFarlin <tabermcfarlin@gmail.com>
2019-02-21 20:13:00 -0500

    hw4 functional

--- hw4/hashit.c
+++ hw4/hashit.c
@@ -7,57 +7,124 @@
  * http://www.spoj.com/problems/HASHIT/
  **************************************************************/
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define TABLE_SIZE 101
-#define KEY_LEN     15
-#define OP_LEN       3
+#define KEY_LEN 15
+#define OP_LEN 3
 
 typedef struct {
-    char* keys[TABLE_SIZE];
-    int num_keys;
+  char *keys[TABLE_SIZE];
+  int num_keys;
 } hash_set;
 
-void clear_table(hash_set *set) {
+hash_set *new_table() {
+  hash_set *set = malloc(sizeof(hash_set));
 
+  for (int i = 0; i < TABLE_SIZE; i++) {
+    set->keys[i] = NULL;
+  }
+
+  set->num_keys = 0;
+}
+
+void clear_table(hash_set *set) {
+  for (int i = 0; i < TABLE_SIZE; i++) {
+    if (set->keys[i]) {
+      free(set->keys[i]);
+    }
+
+    set->keys[i] = NULL;
+  }
+
+  set->num_keys = 0;
 }
 
 int hash(char *key) {
-  int hashVal = 0;
+  int hash_val = 0;
 
   for (int i = 0; key[i] != '\0'; i++) {
-    hashVal = key[i] * (i + 1);
+    hash_val = key[i] * (i + 1);
   }
 
-  return (19 * hashVal) % TABLE_SIZE;
+  return (19 * hash_val) % TABLE_SIZE;
 }
 
 int insert_key(hash_set *set, char *key) {
+  // Get index
+  int hash_val = hash(key);
+  int index = hash_val;
 
-  return 0;
+  // Collision handling
+  int j = 1;
+  while (set->keys[index] && j < 20) {
+    index = (hash_val + j * j + 23 * j) % TABLE_SIZE;
+    j++;
+  }
+
+  // Unhandled collision
+  if (set->keys[index]) {
+    return 0;
+  }
+
+  // Insert key
+  set->keys[index] = strcpy(malloc(sizeof(char) * KEY_LEN), key);
+  set->num_keys++;
+
+  return 1;
 }
 
 int delete_key(hash_set *set, char *key) {
-  
-  return 0;
+  // Get index
+  int hash_val = hash(key);
+  int index = hash_val;
+
+  // Collision handling
+  int j = 1;
+  while (strcmp(set->keys[index], key) != 0 && j < 20) {
+    index = (hash_val + j * j + 23 * j) % TABLE_SIZE;
+    j++;
+  }
+
+  // Unhandled collision
+  if (strcmp(set->keys[index], key) != 0) {
+    return 0;
+  }
+
+  // Delete key
+  free(set->keys[index]);
+  set->keys[index] = NULL;
+  set->num_keys--;
+
+  return 1;
 }
 
 void display_keys(hash_set *set) {
+  printf("%d\n", set->num_keys);
 
+  for (int i = 0; i < TABLE_SIZE; i++) {
+    if (set->keys[i]) {
+      printf("%d:%s\n", i, set->keys[i]);
+    }
+  }
+
+  printf("\n");
 }
 
 int main() {
   // Create hash table
-  hash_set *table = malloc(sizeof(hash_set));
+  hash_set *table = new_table();
 
   // Input
   char *input = malloc((OP_LEN + KEY_LEN) * sizeof(char));
 
   int t;
   for (scanf("%d", &t); t > 0; t--) {
+    clear_table(table);
+
     int n;
     for (scanf("%d", &n); n > 0; n--) {
       scanf("%s", input);
@@ -65,8 +132,17 @@ int main() {
       if (input[0] == 'A') {
         insert_key(table, input + OP_LEN + 1);
       } else if (input[0] = 'D') {
-        insert_key(table, input + OP_LEN + 1);
+        delete_key(table, input + OP_LEN + 1);
       }
     }
+
+    display_keys(table);
   }
+
+  free(input);
+
+  clear_table(table);
+  free(table);
+
+  return 0;
 }
